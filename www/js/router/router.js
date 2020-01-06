3c655d3b5ff5cada43a1e33da608d19df4786785
pankour <p4pankour@outlook.com>
2020-01-06 18:29:09 +0200

    ui changes

---
+++ www/js/router/router.js
@@ -0,0 +1,45 @@
+
+
+var pages = new Array('profile.html', 'leaflet.html', 'profil.html', 'profil.html');
+
+function toggleMenu() {
+    document.getElementsByClassName('wrapper')[0].classList.toggle('menu-open');
+}
+
+function goToPage(page) {
+    console.log(page);
+    var wrapper = document.getElementsByClassName('wrapper')[0];
+    var sections = document.getElementsByTagName('section');
+    for (i = 0; i < sections.length; i++) {
+        sections[i].classList.remove('before', 'after');
+        if (i > page) {
+            sections[i].classList.add('after');
+        }
+    }
+    wrapper.classList.remove('menu-open', 'page-one', 'page-two');
+    console.log(pages[page]);
+    // wrapper.classList.add('page-' + pages[page]);
+    document.getElementById('page_' + page).src = pages[page];
+    // $("#page1").on("click", function (e) {
+    //     console.log(e);
+
+    //     $("#profil").load("profile.html");
+    // });
+
+    // document.getElementById("profil").innerHTML = loadHome();
+    // document.getElementById("transaction").innerHTML = loadTransaction();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
