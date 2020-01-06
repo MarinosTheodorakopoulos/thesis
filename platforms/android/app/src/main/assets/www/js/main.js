0d210fe51d60047aa6233acb2566daff83d9ba77
MarinosThedorakopoulos <marinosthoriginal21@gmail.com>
2020-01-06 22:06:47 +0200

    2252

--- platforms/android/app/src/main/assets/www/js/main.js
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -1,12 +1,12 @@
- (function ($) {
+(function ($) {
     "use strict";
 
 
     /*==================================================================
     [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
@@ -20,13 +20,13 @@
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
@@ -34,20 +34,20 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -68,58 +68,66 @@
     /*==================================================================
     [ Show pass ]*/
     var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
+    $('.btn-show-pass').on('click', function () {
+        if (showPass == 0) {
+            $(this).next('input').attr('type', 'text');
             $(this).find('i').removeClass('zmdi-eye');
             $(this).find('i').addClass('zmdi-eye-off');
             showPass = 1;
         }
         else {
-            $(this).next('input').attr('type','password');
+            $(this).next('input').attr('type', 'password');
             $(this).find('i').addClass('zmdi-eye');
             $(this).find('i').removeClass('zmdi-eye-off');
             showPass = 0;
         }
 
     });
-$('#login').submit(function(e) {
-                    e.preventDefault();
 
-                    var data = {};
-                    var Form = this;
 
-                    $.each(this.elements, function(i, v) {
-                        var input = $(v);
-                        data[input.attr("name")] = input.val();
-                        delete data["undefined"];
-                    });
+    $('#login').submit(function (e) {
+        e.preventDefault();
 
-                    $.ajax({
-                        type: 'POST',
-                        url: 'http://127.0.0.1:5000/login',
-                        dataType: 'json',
-                        contentType: 'application/json; charset=utf-8',
-                        data: JSON.stringify(data),
-                        context: Form,
-                        success: function(callback) {
-				if(callback.fname==""||callback.lname=="")
-					myfunction();
-				else{
+        var data = {};
+        var Form = this;
 
-                            // Watch out for Cross Site Scripting security issues when setting dynamic content!
-                            	$(this).text('Hello ' + callback.fname + ' ' + callback.lname  );
-				window.location.replace("home.html");
-				}
-                        },
-                        error: function() {
-                            $(this).html("error!");
-                        }
-                    });
-                });
+        $.each(this.elements, function (i, v) {
+            var input = $(v);
+            data[input.attr("name")] = input.val();
+            delete data["undefined"];
+        });
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:5000/login',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            context: Form,
+            success: function (callback) {
+                if (callback.fname == "" || callback.lname == "")
+                    myfunction();
+                else {
+                    console.log('Success!')
+                    localStorage.setItem('token', callback.token);
+                    // Watch out for Cross Site Scripting security issues when setting dynamic content!
+                    $(this).text('Hello ' + callback.token + ' ' + callback.token);
+                    if (callback.cid == 1) {
+                        window.location.replace("home1.html");
+                    }
+                    else {
+                        window.location.replace("home2.html");
+                    }
+                }
+            },
+            error: function () {
+                $(this).html("error!");
+            }
+        });
+    });
 })(jQuery);
 function myfunction() {
-  var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
